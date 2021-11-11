af9ac622ce059f025e2d4f0e897f277de72e3a1a
nhan <nhan@gmail.com>
2021-11-11 16:10:37 +0700

    fix template error

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace CapTeam13
+namespace GUI
 {
     public class Program
     {
--- GUI/Startup.cs
+++ GUI/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace CapTeam13
+namespace GUI
 {
     public class Startup
     {
