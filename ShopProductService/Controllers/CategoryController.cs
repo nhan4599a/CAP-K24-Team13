b537c0a9e8855609257224be730493a342a99c4a
Nguyen Dinh Bao <ngdbao1108@gmail.com>
2021-11-22 20:59:45 +0700

    add code for edit category function

---
+++ ShopProductService/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using DatabaseAccessor;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+using System.Threading.Tasks;
+
+namespace ShopProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpPut]
+        [ActionName("Edit")]
+        public async Task<ApiResult<bool>> EditCategory(int id, string CategoryName, int Special)
+        {
+            var category = await _dbContext.ShopCategories.FirstOrDefaultAsync(ct => ct.Id == id);
+            if (category == null)
+            {
+                return new ApiResult<bool> { ResponseCode = 404, ErrorMessage = "Product not found", Data = false };
+            }
+            category.CategoryName = CategoryName;
+            category.Special = Special;
+            var result = await _dbContext.SaveChangesAsync() > 0;
+            if (!result)
+            {
+                return new ApiResult<bool> { ResponseCode = 500, Data = false };
+            }
+            return new ApiResult<bool> { ResponseCode = 200, Data = true };
+        }
+
+    }
+}
