0d77f8370cd19df44b1982e5220ccb8ee6e266ae
nhan4599a <nhan0385790927@gmail.com>
2021-11-22 18:53:29 +0700

    complete del product and adding delete categories

---
+++ GUI/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GUI.Controllers
+{
+    [ApiController]
+    [Route("/api/categories")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
