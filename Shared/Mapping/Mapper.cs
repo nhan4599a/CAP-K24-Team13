4239215ac946c23acb4fdd1def5ec779690b2782
nhan4599a <nhan0385790927@gmail.com>
2021-11-26 16:51:59 +0700

    commit

--- Shared/Mapping/Mapper.cs
+++ Shared/Mapping/Mapper.cs
@@ -26,6 +26,8 @@ namespace Shared.Mapping
                     .ForMember(target => target.Images,
                         options => options.MapFrom(source => source.ImageSet == null ? null :
                             new string[] { source.ImageSet.Image1, source.ImageSet.Image2, source.ImageSet.Image3, source.ImageSet.Image4, source.ImageSet.Image5 }));
+
+                cfg.CreateMap<ShopCategory, CategoryDTO>();
             });
             _mapper = config.CreateMapper();
         }
