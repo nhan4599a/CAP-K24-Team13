20c73297756d8fb528d3fa52d58e99acd2ce37d1
nhan4599a <nhan0385790927@gmail.com>
2021-11-27 10:40:31 +0700

    fix error 400 when add category, fix cors policy

---
+++ ShopProductService/RequestModel/AddCategoryRequestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopProductService.RequestModel
+{
+    public class AddCategoryRequestModel
+    {
+        public string categoryName { get; set; }
+
+        public int special { get; set; }
+    }
+}
