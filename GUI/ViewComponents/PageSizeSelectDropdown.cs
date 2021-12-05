f6bff2479c607d44c7bfa8b6f6af7be490117aaa
nhan4599a <nhan0385790927@gmail.com>
2021-12-05 10:00:15 +0700

    added pagination

---
+++ GUI/ViewComponents/PageSizeSelectDropdown.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GUI.ViewComponents
+{
+    public class PageSizeSelectDropdown : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
