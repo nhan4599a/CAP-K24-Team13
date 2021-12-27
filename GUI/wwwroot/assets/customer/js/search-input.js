e4f1b9b631baaffb5b3c3794f3b471de6d5cb52d
baonguyen110899 <ngdbao1108@gmail.com>
2021-12-27 21:19:05 +0700

    update layout

--- GUI/wwwroot/assets/customer/js/search-input.js
+++
@@ -1,7 +0,0 @@
-﻿
-$("#keyword").on('keyup', function () {
-    let listData = `<li>tìm kiếm sản phẩm</li><li>tìm kiếm cửa hàng</li>`;
-    document.getElementsByClassName(".search-selection").innerHTML = listData;
-    $(".search-selection").show();
-    console.log("typed");
-});
