193d8e5daed8b7badf4f973b281e836de04fa5e1
nhan4599a <nhan0385790927@gmail.com>
2022-01-02 11:58:36 +0700

    add api gateway

--- Service/ShopInterfaceService/Startup.cs
+++ Service/ShopInterfaceService/Startup.cs
@@ -27,6 +27,7 @@ namespace ShopInterfaceService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.RegisterOcelotService(Configuration);
             services.AddControllers();
             services.AddSwaggerGen();
             services.AddScoped<ApplicationDbContext>();
