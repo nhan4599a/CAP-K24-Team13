3402a5dd5b84ca6fa4c0ac0cb01405e9bd889db2
nhan4599a <nhan0385790927@gmail.com>
2022-01-02 12:20:49 +0700

    reorganize

--- AspNetCoreSharedComponent/ServiceDiscoveryHostedService.cs
+++ AspNetCoreSharedComponent/ServiceDiscoveryHostedService.cs
@@ -9,11 +9,15 @@ namespace AspNetCoreSharedComponent
 
         private readonly ServiceConfiguration _serviceConfiguration;
 
+        private readonly ILogger<ServiceDiscoveryHostedService> _logger;
+
         private readonly string _serviceRegistrationId;
 
-        public ServiceDiscoveryHostedService(IConsulClient consulClient, IConfiguration configuration)
+        public ServiceDiscoveryHostedService(IConsulClient consulClient, IConfiguration configuration,
+            ILogger<ServiceDiscoveryHostedService> logger)
         {
             _consulClient = consulClient;
+            _logger = logger;
             _serviceConfiguration = configuration.GetServiceConfiguration();
             _serviceRegistrationId = $"{_serviceConfiguration.ServiceName}-{_serviceConfiguration.ServiceId}";
         }
@@ -27,8 +31,14 @@ namespace AspNetCoreSharedComponent
                 Address = _serviceConfiguration.ServiceAddress.Host,
                 Port = _serviceConfiguration.ServiceAddress.Port
             };
-            await _consulClient.Agent.ServiceDeregister(_serviceRegistrationId, cancellationToken);
-            await _consulClient.Agent.ServiceRegister(registration, cancellationToken);
+            try
+            {
+                await _consulClient.Agent.ServiceDeregister(_serviceRegistrationId, cancellationToken);
+                await _consulClient.Agent.ServiceRegister(registration, cancellationToken);
+            }catch (Exception ex)
+            {
+                _logger.LogError("Failed to register service {0}, inner exception is: {1}", registration.Name, ex.Message);
+            }
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
