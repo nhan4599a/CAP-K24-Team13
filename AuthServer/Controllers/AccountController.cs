eddd56f3006ce0f5da66283d59662bf5489645b4
nhan4599a <nhan0385790927@gmail.com>
2022-01-20 18:49:39 +0700

    edit information

---
+++ AuthServer/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using AuthServer.Identities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationUserManager _userManager;
+
+        public AccountController(ApplicationUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        public IActionResult Information()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ActionName("Information")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditInformation()
+        {
+            return View();
+        }
+    }
+}
