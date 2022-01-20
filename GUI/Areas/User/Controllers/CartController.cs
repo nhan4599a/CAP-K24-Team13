046dc908412f3bb0579ebbe00e48e92c87eaa4e7
nhan4599a <nhan0385790927@gmail.com>
2022-01-20 12:07:31 +0700

    fix cart

--- GUI/Areas/User/Controllers/CartController.cs
+++ GUI/Areas/User/Controllers/CartController.cs
@@ -1,5 +1,4 @@
 ﻿using GUI.Attributes;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GUI.Areas.User.Controllers
@@ -7,7 +6,6 @@ namespace GUI.Areas.User.Controllers
     [VirtualArea("User")]
     public class CartController : Controller
     {
-        [Authorize]
         public IActionResult Index()
         {
             return View();
