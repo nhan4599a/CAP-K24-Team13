6d0a90071e023bd1a8ad0cfbc982cb3d01594b86
nhan4599a <nhan0385790927@gmail.com>
2022-01-25 19:27:55 +0700

    fix cart

--- GUI/wwwroot/assets/user-official/js/cart.js
+++ GUI/wwwroot/assets/user-official/js/cart.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     $('.product > .product-media > .product-action > a.btn-product.btn-cart').click(function () {
         let productId = $(this).data('product');
-        let userId = '';
+        let userId = 'adf66c79-e39a-4288-fcf9-08d9df112449';
         addProductToCart(userId, productId)
             .then(() => toastr.success('Added product to cart'))
             .catch(error => toastr.error(error));
