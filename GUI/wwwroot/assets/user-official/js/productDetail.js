f91c0a1265ec288e62ec4430a1fcdfa201bebf1f
187PM20483 - Phan Le Huy - K24T-PM1 <huy.187pm20483@vanlanguni.vn>
2022-01-26 22:21:53 +0700

    transfer image product detail

---
+++ GUI/wwwroot/assets/user-official/js/productDetail.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(() => {
+    $('.product-gallery > #product-zoom-gallery > .product-gallery-item').click(function (e) {
+        e.preventDefault();
+        let image = $(this).data('image');
+        $(this).parent().parent().children('figure').children().attr('src', image);
+    })
+})
