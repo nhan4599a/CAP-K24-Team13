8e253227bcac0671617efd738a9a43d8c672db45
buiphuoctai145 <55799927+buiphuoctai145@users.noreply.github.com>
2022-01-29 19:05:41 +0700

    user account by Tai

---
+++ GUI/Areas/User/Controllers/UserAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GUI.Areas.User.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
