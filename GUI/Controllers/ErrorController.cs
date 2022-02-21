30faa5fa9c32507f5ddb2f8373fbfb4525121ef6
nhan4599a <nhan0385790927@gmail.com>
2022-02-21 23:25:46 +0700

    debug error page

--- GUI/Controllers/ErrorController.cs
+++ GUI/Controllers/ErrorController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using Microsoft.Extensions.Logging;
 
 namespace GUI.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             var responseCode = HttpContext.Session.GetInt32("ResponseCode");
+            _logger.LogInformation("Error, Response Code = " + responseCode);
             if (responseCode.HasValue)
                 HttpContext.Session.Remove("ResponseCode");
             responseCode = (!responseCode.HasValue || (responseCode != 404 && responseCode != 500)) ? 500 : responseCode;
