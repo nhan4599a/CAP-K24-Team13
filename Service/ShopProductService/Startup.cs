0900011628221a00bafcab6b26f04d457d16184d
nhan4599a <nhan0385790927@gmail.com>
2022-02-21 21:19:37 +0700

    fix link

--- Service/ShopProductService/Startup.cs
+++ Service/ShopProductService/Startup.cs
@@ -41,7 +41,7 @@ namespace ShopProductService
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
                 {
-                    options.Authority = "https://localhost:7265";
+                    options.Authority = "http://ec2-52-207-214-39.compute-1.amazonaws.com:7265";
                     options.Audience = "product";
                 });
             services.AddMediatR(typeof(Startup));
