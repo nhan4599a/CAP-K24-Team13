29488d8a9379c95bfcd99c496db32217e5f0099a
nhan4599a <nhan0385790927@gmail.com>
2022-02-23 20:05:54 +0700

    fix links

--- GUI/Startup.cs
+++ GUI/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
