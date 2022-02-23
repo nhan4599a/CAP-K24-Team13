8239d2f89427eb39900d50e773629b9d15730275
187PM20483 - Phan Le Huy - K24T-PM1 <huy.187pm20483@vanlanguni.vn>
2022-02-23 19:48:06 +0700

    try to fix login

--- GUI/Startup.cs
+++ GUI/Startup.cs
@@ -103,6 +103,11 @@ namespace GUI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Use((context, next) =>
+            {
+                context.Request.Scheme = "https";
+                return next();
+            });
             app.UseSession();
             app.UseCookiePolicy(new CookiePolicyOptions
             {
