f7b4cde251ae7730108e178bb3ef958575800072
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2022-03-08 16:22:24 +0700

    try

--- Service/StatisticService/Handlers/OrderStatisticCommandHandler.cs
+++ Service/StatisticService/Handlers/OrderStatisticCommandHandler.cs
@@ -41,52 +41,53 @@ namespace StatisticService.Handlers
                         $"SELECT Invoices.* FROM Invoices, InvoiceDetails WHERE Invoices.Id = InvoiceDetails.InvoiceId AND YEAR(Invoices.CreatedAt) = {currentYear}"
                     );
             }
-            var groupingResult = invoices.GroupBy(invoice => invoice.CreatedAt).AsNoTracking();
-            var statisticResultItems = new List<StatisticResultItem>();
-            var highestIncome = 0d;
-            var lowestIncome = double.MaxValue;
-            var highestDate = DateTime.Now;
-            var lowestDate = DateTime.Now;
-            foreach (var group in groupingResult)
-            {
-                var invoiceList = group.ToList();
-                var newInvoiceList = group.Where(invoice => invoice.Status == InvoiceStatus.New).ToList();
-                var confirmedInvoiceList = group.Where(invoice => invoice.Status == InvoiceStatus.Confirmed).ToList();
-                var estimatedIncome = group.Where(invoice => invoice.Status <= InvoiceStatus.Confirmed)
-                        .Sum(invoice => invoice.Details.Sum(detail => detail.Price));
-                var actualIncome = confirmedInvoiceList.Sum(invoice => invoice.Details.Sum(detail => detail.Price));
-                if (actualIncome > highestIncome)
-                {
-                    highestIncome = actualIncome;
-                    highestDate = group.Key;
-                }
-                if (actualIncome < lowestIncome)
-                {
-                    lowestIncome = actualIncome;
-                    lowestDate = group.Key;
-                }
-                statisticResultItems.Add(new StatisticResultItem
-                {
-                    Data = new StatisticResultItemData
-                    {
-                        Total = invoiceList.Count,
-                        NewInvoiceCount = newInvoiceList.Count,
-                        ConfirmedInvoiceCount = confirmedInvoiceList.Count,
-                        CanceledInvoiceCount = invoiceList.Count(invoice => invoice.Status == InvoiceStatus.Canceled)
-                    },
-                    EstimatedIncome = estimatedIncome,
-                    ActualIncome = actualIncome
-                });
-            }
-            var statisticResult = new StatisticResult<Invoice>(request.Strategy)
-            {
-                Details = statisticResultItems.ToArray(),
-                HighestIncome = highestIncome,
-                LowestIncome = lowestIncome,
-                HighestDate = new StatisticDateResult(request.Strategy, highestDate.ToDateOnly()),
-                LowestDate = new StatisticDateResult(request.Strategy, lowestDate.ToDateOnly())
-            };
-            return Task.FromResult(statisticResult);
+            //var groupingResult = invoices.GroupBy(invoice => invoice.CreatedAt).AsNoTracking();
+            //var statisticResultItems = new List<StatisticResultItem>();
+            //var highestIncome = 0d;
+            //var lowestIncome = double.MaxValue;
+            //var highestDate = DateTime.Now;
+            //var lowestDate = DateTime.Now;
+            //foreach (var group in groupingResult)
+            //{
+            //    var invoiceList = group.ToList();
+            //    var newInvoiceList = group.Where(invoice => invoice.Status == InvoiceStatus.New).ToList();
+            //    var confirmedInvoiceList = group.Where(invoice => invoice.Status == InvoiceStatus.Confirmed).ToList();
+            //    var estimatedIncome = group.Where(invoice => invoice.Status <= InvoiceStatus.Confirmed)
+            //            .Sum(invoice => invoice.Details.Sum(detail => detail.Price));
+            //    var actualIncome = confirmedInvoiceList.Sum(invoice => invoice.Details.Sum(detail => detail.Price));
+            //    if (actualIncome > highestIncome)
+            //    {
+            //        highestIncome = actualIncome;
+            //        highestDate = group.Key;
+            //    }
+            //    if (actualIncome < lowestIncome)
+            //    {
+            //        lowestIncome = actualIncome;
+            //        lowestDate = group.Key;
+            //    }
+            //    statisticResultItems.Add(new StatisticResultItem
+            //    {
+            //        Data = new StatisticResultItemData
+            //        {
+            //            Total = invoiceList.Count,
+            //            NewInvoiceCount = newInvoiceList.Count,
+            //            ConfirmedInvoiceCount = confirmedInvoiceList.Count,
+            //            CanceledInvoiceCount = invoiceList.Count(invoice => invoice.Status == InvoiceStatus.Canceled)
+            //        },
+            //        EstimatedIncome = estimatedIncome,
+            //        ActualIncome = actualIncome
+            //    });
+            //}
+            //var statisticResult = new StatisticResult<Invoice>(request.Strategy)
+            //{
+            //    Details = statisticResultItems.ToArray(),
+            //    HighestIncome = highestIncome,
+            //    LowestIncome = lowestIncome,
+            //    HighestDate = new StatisticDateResult(request.Strategy, highestDate.ToDateOnly()),
+            //    LowestDate = new StatisticDateResult(request.Strategy, lowestDate.ToDateOnly())
+            //};
+            //return Task.FromResult(statisticResult);
+            return Task.FromResult<StatisticResult<Invoice>>(null);
         }
 
         public void Dispose()
