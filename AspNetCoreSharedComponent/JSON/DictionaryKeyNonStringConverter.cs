a802c4df4a4d37f02cc07e7c97fae5bbdcf8ed24
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2022-03-08 20:10:59 +0700

    doing statistic

--- AspNetCoreSharedComponent/JSON/DictionaryKeyNonStringConverter.cs
+++ AspNetCoreSharedComponent/JSON/DictionaryKeyNonStringConverter.cs
@@ -9,7 +9,14 @@ using System.Text.Json.Serialization;
 namespace AspNetCoreSharedComponent.JSON
 {
     public class DictionaryKeyNonStringConverter<TKey> : JsonConverter<IDictionary<TKey, object>>
-    {   
+    {
+        private readonly ILogger _logger;
+
+        public DictionaryKeyNonStringConverter(ILogger logger)
+        {
+            _logger = logger;
+        }
+            
         public override IDictionary<TKey, object>? Read(ref Utf8JsonReader reader,
             Type typeToConvert, JsonSerializerOptions options)
         {
@@ -18,6 +25,7 @@ namespace AspNetCoreSharedComponent.JSON
 
         public override void Write(Utf8JsonWriter writer, IDictionary<TKey, object> value, JsonSerializerOptions options)
         {
+            _logger.LogInformation("Writing json");
             var convertedDictionary = new Dictionary<string, object>(value.Count);
             foreach (var (k, v) in value) convertedDictionary[k!.ToString()!] = v;
             JsonSerializer.Serialize(writer, convertedDictionary, options);
@@ -37,9 +45,7 @@ namespace AspNetCoreSharedComponent.JSON
             {
                 if (!typeToConvert.IsGenericType) return false;
                 if (typeToConvert.GenericTypeArguments[0] == typeof(string)) return false;
-                _logger.LogInformation("typeToConvert: {0}", typeToConvert.FullName);
                 var @interface = typeToConvert.GetInterface("IDictionary");
-                _logger.LogInformation("canConvert: {0}", @interface != null);
                 return @interface != null;
             }
 
@@ -51,7 +57,7 @@ namespace AspNetCoreSharedComponent.JSON
                     converterType,
                     BindingFlags.Instance | BindingFlags.Public,
                     null,
-                    null,
+                    _logger,
                     CultureInfo.CurrentCulture)!;
                 return converter;
             }
