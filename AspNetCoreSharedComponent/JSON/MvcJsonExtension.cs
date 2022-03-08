ac92619f39c1356111da0d23168d8c987017280c
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2022-03-08 18:59:59 +0700

    doing statistic

--- AspNetCoreSharedComponent/JSON/MvcJsonExtension.cs
+++ AspNetCoreSharedComponent/JSON/MvcJsonExtension.cs
@@ -9,7 +9,7 @@ namespace AspNetCoreSharedComponent.JSON
             builder.AddJsonOptions(options =>
             {
                 options.JsonSerializerOptions.Converters.Insert(0, new JsonPropertyToStringConverter<T>());
-                options.JsonSerializerOptions.Converters.Add(new DictionaryKeyNonStringConverter<T>());
+                options.JsonSerializerOptions.Converters.Insert(0, new DictionaryKeyNonStringConverter<T>());
             });
             return builder;
         }
