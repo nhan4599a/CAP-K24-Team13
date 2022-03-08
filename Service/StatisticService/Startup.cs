fbda62164891a10fe035dc736ed1bf9d78f1dc24
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2022-03-08 20:02:00 +0700

    doing statistic

--- Service/StatisticService/Startup.cs
+++ Service/StatisticService/Startup.cs
@@ -17,7 +17,7 @@ namespace StatisticService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
