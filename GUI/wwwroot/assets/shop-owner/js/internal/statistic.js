476e6a3f2238448d1e901cad13bef3f30fe561d2
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2022-03-11 18:01:18 +0700

    integrating statistic

--- GUI/wwwroot/assets/shop-owner/js/internal/statistic.js
+++ GUI/wwwroot/assets/shop-owner/js/internal/statistic.js
@@ -1,20 +1,53 @@
 ﻿$(document).ready(() => {
-    
-    const context = document.getElementById('statistic-chart').getContext('2d');
-    const chart = new Chart(context, {
-        type: 'line',
-        data: data,
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {
-                    position: 'top'
-                },
-                title: {
-                    display: true,
-                    text: 'Orders statistic chart'
-                }
-            }
-        }
-    });
+    const chart = null;
+    let animationLoader = new AnimationLoader('#loading-container > #animation-container', '/assets/shop-owner/img/illustrations/loading.json');
+    animationLoader.showAnimation();
+    getShopId()
+        .then(shopId => {
+            getStatisticOfShop(shopId, 'ByDay')
+                .then(statisticResult => {
+                    animationLoader.hideAnimation();
+                    const context = document.getElementById('statistic-chart').getContext('2d');
+                    const data = Object.entries(statisticResult)
+                        .map(keyValuePair => {
+                            return {
+                                date: keyValuePair[0],
+                                actualIncome: keyValuePair[1].actualIncome,
+                                estimatedIncome: keyValuePair[1].estimatedIncome
+                            };
+                        });
+                    chart = new Chart(context, {
+                        type: 'line',
+                        data: {
+                            labels: data.map(obj => obj.date),
+                            datasets: [
+                                {
+                                    label: 'Actual Income',
+                                    data: data.map(obj => obj.actualIncome)
+                                },
+                                {
+                                    label: 'Estimated Income',
+                                    data: data.map(obj => obj.estimatedIncome)
+                                }
+                            ]
+                        },
+                        options: {
+                            responsive: true,
+                            plugins: {
+                                legend: {
+                                    position: 'top'
+                                },
+                                title: {
+                                    display: true,
+                                    text: 'Orders statistic chart'
+                                }
+                            }
+                        }
+                    });
+                })
+                .catch(error => {
+                    animationLoader.hideAnimation();
+                    toastr.error(error, 'Error!');
+                });
+        });
 });
