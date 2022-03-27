c81c46d6c66dbe85a4563f527300d1387e08c7fb
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2022-03-27 18:53:13 +0700

    fix bug on checkout

--- DatabaseAccessor/Triggers/InvoiceStatusChangedTrigger.cs
+++ DatabaseAccessor/Triggers/InvoiceStatusChangedTrigger.cs
@@ -1,49 +1,68 @@
-﻿using DatabaseAccessor.Models;
+﻿using DatabaseAccessor.Contexts;
+using DatabaseAccessor.Models;
 using EntityFrameworkCore.Triggered;
 using Shared.Models;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace DatabaseAccessor.Triggers
 {
-    internal class InvoiceStatusChangedTrigger : IBeforeSaveTrigger<Invoice>
+    public class InvoiceStatusChangedTrigger : IBeforeSaveTrigger<Invoice>, IAfterSaveTrigger<Invoice>, IDisposable
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public InvoiceStatusChangedTrigger(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AfterSave(ITriggerContext<Invoice> context, CancellationToken cancellationToken)
+        {
+            if (context.ChangeType == ChangeType.Added)
+            {
+                var invoice = await _dbContext.Invoices.FindAsync(new object[] { context.Entity.Id }, cancellationToken: cancellationToken);
+                invoice.StatusChangedHistories.Add(new InvoiceStatusChangedHistory
+                {
+                    OldStatus = null,
+                    NewStatus = InvoiceStatus.New
+                });
+                await _dbContext.SaveChangesAsync(cancellationToken);
+            }
+        }
+
         public Task BeforeSave(ITriggerContext<Invoice> context, CancellationToken cancellationToken)
         {
-            if (context.ChangeType == ChangeType.Modified || context.ChangeType == ChangeType.Added)
+            if (context.ChangeType == ChangeType.Modified)
             {
-                if (context.ChangeType == ChangeType.Added)
+                context.Entity.StatusChangedHistories.Add(new InvoiceStatusChangedHistory
                 {
-                    context.Entity.StatusChangedHistories.Add(new InvoiceStatusChangedHistory
-                    {
-                        NewStatus = InvoiceStatus.New
-                    });
-                }
-                else
+                    OldStatus = context.UnmodifiedEntity.Status,
+                    NewStatus = context.Entity.Status
+                });
+                if (context.Entity.Status == InvoiceStatus.Confirmed && context.UnmodifiedEntity.Status == InvoiceStatus.New)
                 {
-                    context.Entity.StatusChangedHistories.Add(new InvoiceStatusChangedHistory
+                    foreach (var detail in context.Entity.Details)
                     {
-                        OldStatus = context.UnmodifiedEntity.Status,
-                        NewStatus = context.Entity.Status
-                    });
-                    if (context.Entity.Status == InvoiceStatus.Confirmed && context.UnmodifiedEntity.Status == InvoiceStatus.New)
-                    {
-                        foreach (var detail in context.Entity.Details)
-                        {
-                            detail.Product.Quantity -= detail.Quantity;
-                        }
+                        detail.Product.Quantity -= detail.Quantity;
                     }
+                }
 
-                    if (context.Entity.Status == InvoiceStatus.Canceled)
+                if (context.Entity.Status == InvoiceStatus.Canceled)
+                {
+                    foreach (var detail in context.Entity.Details)
                     {
-                        foreach (var detail in context.Entity.Details)
-                        {
-                            detail.Product.Quantity += detail.Quantity;
-                        }
+                        detail.Product.Quantity += detail.Quantity;
                     }
                 }
             }
             return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+            GC.SuppressFinalize(this);
         }
     }
 }
