06b700518e1925f5e6c44ead72c1988437100a69
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2022-04-13 15:31:18 +0700

    switch deploy place

--- GUI/wwwroot/assets/shop-owner/js/internal/api.js
+++ GUI/wwwroot/assets/shop-owner/js/internal/api.js
@@ -1,5 +1,5 @@
 ﻿axios.defaults.timeout = 20000;
-axios.defaults.baseURL = 'http://ec2-52-207-214-39.compute-1.amazonaws.com:3000';
+axios.defaults.baseURL = 'http://ec2-18-139-226-54.ap-southeast-1.compute.amazonaws.com:3000';
 
 axios.interceptors.request.use(async config => {
     if (config.url.startsWith('/products', '/categories') && config.method == 'get')
