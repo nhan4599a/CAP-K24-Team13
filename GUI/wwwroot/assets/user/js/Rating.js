21ca4037dc372fbde19c7748d725176f51e4a018
Ho Nguyen Tri Nhan <nhan0385790927@gmail.com>
2022-04-20 19:12:21 +0700

    fix rating product

--- GUI/wwwroot/assets/user/js/Rating.js
+++ GUI/wwwroot/assets/user/js/Rating.js
@@ -8,13 +8,17 @@
         modal.find('.btn.btn-primary').click(function () {
             let star = $('input[name=rating]:checked').val();
             let comment = $('#comment').val();
+            let animationLoader = new AnimationLoader('#loading-container > #animation-container', '/assets/user/checking-out.json');
+            animationLoader.showAnimation();
             getUserId().then(userId => {
                 ratingProduct(userId, productId, star, comment)
                     .then(() => {
+                        animationLoader.hideAnimation();
                         toastr.success('Rating Success');
                         window.location.href = `/product/index/${productId}`;
                     })
-                    .catch((error) => {
+                    .catch(error => {
+                        animationLoader.hideAnimation();
                         toastr.error(error);
                     });
             });
